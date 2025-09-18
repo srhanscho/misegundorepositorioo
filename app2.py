cb1fd55aeea4a1c0966842b6bee73926bf22b92f
sebaszzz25 <sebasmejia2009.sm@gmail.com>
2025-09-18 11:48:05 -0500

    nada

--- app2.py
+++ app2.py
@@ -3,6 +3,6 @@ sebastian=str(input("Quien eres"))
 while True:
     if sebastian == "Sebastian" or "sebastian":
         print("Puede Pasar")
-        break
     else:
         print("IMPOSTOR❌❌❌❌❌❌❌❌ \n ACCESO DENEGADO")
+    break
