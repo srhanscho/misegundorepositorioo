cd525f71c882d3a409340a2c6205fc61930625d4
Han's de Ávila <144625014+srhanscho@users.noreply.github.com>
2025-09-18 11:43:06 -0500

    acceso sebastian

--- app2.py
+++ app2.py
@@ -1,1 +1,8 @@
 print("Sabroso soy")
+sebastian=str(input("Quien eres"))
+while True:
+    if sebastian == "Sebastian" or "sebastian":
+        print("Puede Pasar")
+        break
+    else:
+        print("IMPOSTOR❌❌❌❌❌❌❌❌ \n ACCESO DENEGADO")
